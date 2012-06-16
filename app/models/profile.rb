28f34bc36abc2d3d17292649a684ab0c51847fca
jodyalbritton <jody.albritton@gmail.com>
2012-06-16 04:12:15 -0500

    modified:   app/assets/stylesheets/application.css 	modified:   app/models/profile.rb 	modified:   app/views/activities/_comment.html.erb 	modified:   app/views/activities/_ingredient.html.erb 	modified:   app/views/activities/_post.html.erb 	modified:   app/views/activities/_stat.html.erb 	modified:   app/views/devise/registrations/edit.html.erb 	modified:   app/views/opinio/comments/_comment.html.erb 	new file:   public/system/profiles/avatars/000/000/001/medium/472263_3737704135472_156739474_o.jpg 	new file:   public/system/profiles/avatars/000/000/001/mini/472263_3737704135472_156739474_o.jpg 	new file:   public/system/profiles/avatars/000/000/001/original/472263_3737704135472_156739474_o.jpg 	new file:   public/system/profiles/avatars/000/000/001/thumb/472263_3737704135472_156739474_o.jpg

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,11 @@
 class Profile < ActiveRecord::Base
-  attr_accessible :avatar, :fname, :lname, :dob, :about
+  attr_accessible :fname, :lname, :dob, :about, :avatar
   
   
   has_attached_file :avatar, 
-          :styles => { :medium => "100x100>", :thumb => "32x32>" },
+          :styles => { :medium => "300x300>", 
+          :thumb => "64x64>",
+          :mini => "32x32>" },
           :default_url =>"/assets/missing_:style.png"
   
 end
