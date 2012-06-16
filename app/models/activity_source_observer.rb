215fc0764bfc7502392c057ffe8796ff8aa12f57
jodyalbritton <jody.albritton@gmail.com>
2012-06-15 22:22:16 -0500

    bug fix

--- app/models/activity_source_observer.rb
+++ app/models/activity_source_observer.rb
@@ -2,7 +2,7 @@ class ActivitySourceObserver < ActiveRecord::Observer
  
  
  
- observe :post, :comment, :stat
+ observe :post, :comment, :stat, :ingredient
 
   def after_create(target)
    @object = target.class.to_s
@@ -27,6 +27,26 @@ class ActivitySourceObserver < ActiveRecord::Observer
       :target_type => target.class.to_s,
       :parent_id => target.commentable_id,
       :verb => "commented")
+      
+  elsif @object == "Stat"
+      Activity.create!(
+      :user => target.user, 
+      :target_id => target.id, 
+      :activity_type=> target.class.to_s, 
+      :created_at => target.created_at, 
+      :updated_at => target.updated_at,
+      :target_type => target.class.to_s, 
+      :verb => "tracked")
+      
+  elsif @object == "Ingredient"
+      Activity.create!(
+      :user => target.user, 
+      :target_id => target.id, 
+      :activity_type=> target.class.to_s, 
+      :created_at => target.created_at, 
+      :updated_at => target.updated_at,
+      :target_type => target.class.to_s, 
+      :verb => "logged")
       
    
    end
