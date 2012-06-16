812247efad00936134ffef7924eaba522da0b45f
jodyalbritton <jody.albritton@gmail.com>
2012-06-15 22:27:44 -0500

    deleted:    app/controllers/streams_controller.rb 	deleted:    app/models/activity_object.rb 	deleted:    app/models/activity_verb.rb 	deleted:    app/views/activities/_activity.html.erb 	deleted:    app/views/home/_mpost.html.erb 	deleted:    app/views/home/_mposts.html.erb 	deleted:    app/views/home/_mposts.html.old 	deleted:    app/views/home/_post.html.erb 	deleted:    app/views/home/index.js.erb 	deleted:    app/views/posts/_posts.html 	deleted:    app/views/shared/_comment_form.html.erb 	deleted:    app/views/shared/create.js.erb 	deleted:    app/views/streams/index.html.erb

--- app/models/activity_object.rb
+++
@@ -1,4 +0,0 @@
-class ActivityObject < ActiveRecord::Base
-  belongs_to :activity
-  # attr_accessible :title, :body
-end
--- app/models/activity_verb.rb
+++
@@ -1,3 +0,0 @@
-class ActivityVerb < ActiveRecord::Base
-  attr_accessible :name
-end
