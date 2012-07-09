22daab406ad518371d2119fdb915815986995a02
jodyalbritton <jody.albritton@gmail.com>
2012-07-09 02:29:51 -0500

    fix

--- db/migrate/20120706203448_add_audience_member_type_to_audiences.rb
+++
@@ -1,5 +0,0 @@
-class AddAudienceMemberTypeToAudiences < ActiveRecord::Migration
-  def change
-    add_column :audiences, :audience_member_type, :string
-  end
-end
--- db/migrate/20120707225522_add_post_to_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddPostToTags < ActiveRecord::Migration
-  def change
-    add_column :tags, :post, :integer
-  end
-end
