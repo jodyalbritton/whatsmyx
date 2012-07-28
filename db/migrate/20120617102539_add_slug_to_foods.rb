aae7fca1ce2528cd993ac540ffdd5066ab683e0e
jodyalbritton <jody.albritton@gmail.com>
2012-07-28 04:29:09 -0500

    remove some migs

--- db/migrate/20120617102539_add_slug_to_foods.rb
+++
@@ -1,5 +0,0 @@
-class AddSlugToFoods < ActiveRecord::Migration
-  def change
-    add_column :foods, :slug, :string
-  end
-end
--- db/migrate/20120617103736_add_index_to_foods.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToFoods < ActiveRecord::Migration
-  def change
- 
-    add_index :foods, :slug, unique: true
-  end
-end
