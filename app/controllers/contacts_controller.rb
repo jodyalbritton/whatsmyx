f16fb84887f4aee858182430faec0396d1a3bf2d
jodyalbritton <jody.albritton@gmail.com>
2012-07-31 15:17:03 -0500

    fixed circles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,10 @@
 class ContactsController < ApplicationController
   
   def index
-  @users = User.where("name like ?", "%#{params[:q]}%")
+  @users = User.where("username like ?", "%#{params[:q]}%")
   respond_to do |format|
     format.html
-    format.json { render :json => @users.map(&:attributes) }
+    format.json { render :json => @users.collect {|user| {:id => user.id, :name => user.username } }}
   end
 end
 
