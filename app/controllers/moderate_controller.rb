e61ae52320bba26d899f659ccec8a4c0da76fce9
jodyalbritton <jody.albritton@gmail.com>
2012-07-31 13:48:04 -0500

    fixed moderation

--- app/controllers/moderate_controller.rb
+++ app/controllers/moderate_controller.rb
@@ -1,6 +1,8 @@
 class ModerateController < ApplicationController
+   
   def index 
-    @users = User.where(:confirmed_at == nil)
+    authorize! :index, @user, :message => 'Not authorized as an administrator.'
+    @users = User.where(:confirmed_at == "")
   end
   
 
