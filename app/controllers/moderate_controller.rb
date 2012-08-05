c9425d46744b91c7be07934e25fcdd7c8a102dbb
jodyalbritton <jody.albritton@gmail.com>
2012-08-05 18:15:34 -0500

    more moderation

--- app/controllers/moderate_controller.rb
+++ app/controllers/moderate_controller.rb
@@ -1,8 +1,9 @@
 class ModerateController < ApplicationController
-   
+  before_filter :authenticate_user!
   def index 
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.where(:confirmed_at == "")
+    @users = User.where(:confirmation_sent_at => nil)
+    @current_users = User.all
   end
   
 
