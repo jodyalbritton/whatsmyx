56d78badc02f713624ce714932f969d46b42db31
jodyalbritton <jody.albritton@gmail.com>
2012-08-05 21:50:31 -0500

    fixed nutrition

--- app/controllers/nutrition_controller.rb
+++ app/controllers/nutrition_controller.rb
@@ -2,23 +2,35 @@
     before_filter :authenticate_user!
   def index
   @meal = current_user.meals.build 
-  @date = params[:date] ? Date.parse(params[:date]) : Date.today
+  
   @user = User.find(current_user)
   
   
- 
+  if params["date(1i)"]
+  @somedate = DateTime.new(params["date(1i)"].to_i, 
+                           params["date(2i)"].to_i,
+                           params["date(3i)"].to_i)
+    
+  elsif params[:date]
+  @somedate = Date.parse(params[:date]) 
+  else 
+  @somedate = Date.today
+  end 
  
   
     
-  if params[:date] 
-  @meals = @user.meals.where(:date => params[:date]).order("created_at ASC")
-  @meals_tot = @user.meals.where(:date => params[:date])
-  @daily_activities = @user.pactivities.where(:date => params[:date])
-  if  @user.profile == nil
-  @daily_burn = 0
-  elsif
-  @daily_burn = (@daily_activities.sum { |daily| daily.burned }) + @user.profile.mbr
-  end 
+
+  
+  @meals = @user.meals.where(:date => @somedate).order("created_at ASC")
+  @meals_tot = @user.meals.where(:date => @somedate)
+  @daily_activities = @user.pactivities.where(:date => @somedate)
+   
+    
+      if  @user.profile == nil
+        @daily_burn = 0
+      elsif
+      @daily_burn = (@daily_activities.sum { |daily| daily.burned }) + @user.profile.mbr
+      end 
   @tot_cals = @meals.sum { |meal| meal.cals }
   @tot_fats = @meals.sum { |meal| meal.fats }
   @tot_prot = @meals.sum { |meal| meal.prot }
@@ -30,26 +42,8 @@
   @tot_fibr = @meals.sum { |meal| meal.fibr }
   @tot_satf = @meals.sum { |meal| meal.satf }
  
-  else
-  @meals = @user.meals.where(:date => Date.today).order("created_at ASC")
-  @meals_tot = @user.meals.where(:date => Date.today)
-  @daily_activities = @user.pactivities.where(:date => Date.today)
-  if  @user.profile == nil
-  @daily_burn = 0
-  elsif
-  @daily_burn = (@daily_activities.sum { |daily| daily.burned }) + @user.profile.mbr
-  end 
-  @tot_cals = @meals.sum { |meal| meal.cals }
-  @tot_fats = @meals.sum { |meal| meal.fats }
-  @tot_prot = @meals.sum { |meal| meal.prot }
-  @tot_carb = @meals.sum { |meal| meal.carb }
-  @tot_sugs = @meals.sum { |meal| meal.sugs }
-  @tot_pots = @meals.sum { |meal| meal.pots }
-  @tot_chol = @meals.sum { |meal| meal.chol }
-  @tot_sodi = @meals.sum { |meal| meal.sodi }
-  @tot_fibr = @meals.sum { |meal| meal.fibr }
-  @tot_satf = @meals.sum { |meal| meal.satf }
-  end
+  
+  
   
  
   end 
