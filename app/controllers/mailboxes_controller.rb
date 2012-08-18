0266433e740c7160b3c2cd1204eacc00d3c324c1
jodyalbritton <jody.albritton@gmail.com>
2012-08-18 16:27:25 -0500

    messaging system

--- app/controllers/mailboxes_controller.rb
+++ app/controllers/mailboxes_controller.rb
@@ -4,5 +4,6 @@ class MailboxesController < ApplicationController
      @messages = Recipient.where(:user_id => @user)
      @sent_messages = Message.where(:sender_id => @user.id)
      @notifications = Notification.where(:user_id => @user.id)
+     @message = current_user.messages.build(params[:message])
   end
 end
