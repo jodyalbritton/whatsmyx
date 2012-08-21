38f0eb7c0e95c46d9af9d80e10eb637a58fb1174
jodyalbritton <jody.albritton@gmail.com>
2012-08-21 06:40:50 -0500

    modified:   app/assets/stylesheets/application.css.less 	modified:   app/controllers/application_controller.rb 	modified:   app/controllers/home_controller.rb 	modified:   app/views/layouts/_navigation.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/mailboxes/show.html.erb 	modified:   app/views/users/show.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
    protect_from_forgery
    rescue_from CanCan::AccessDenied do |exception|
    redirect_to root_path, :alert => exception.message
+   
+    @global_messages = Recipient.where(:user_id => current_user)
+    @global_notifications = current_user.notifications
  
   end
 end
