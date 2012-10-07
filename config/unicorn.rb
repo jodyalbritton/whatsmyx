970f6b7dde308f922bf8e28ab49e51d1fdb84db7
jodyalbritton <jody.albritton@gmail.com>
2012-10-07 18:50:28 -0500

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 working_directory "/var/src/whatsmyx"
 pid "/var/src/whatsmyx/tmp/pids/unicorn.pid"
-stderr_path "/var/src/log/unicorn.log"
+stderr_path "/var/src/whatsmyx/log/unicorn.log"
 stdout_path "/var/src/whatsmyx/log/unicorn.log"
 
 listen "var/src/whatsmyx/tmp/sockets/unicorn.sock"
