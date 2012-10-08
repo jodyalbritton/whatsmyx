aa90be72d46e5ae52279dc03c478c63719b3d61b
jodyalbritton <jody.albritton@gmail.com>
2012-10-07 20:17:30 -0500

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid "/var/src/whatsmyx/tmp/pids/unicorn.pid"
 stderr_path "/var/src/whatsmyx/log/unicorn.log"
 stdout_path "/var/src/whatsmyx/log/unicorn.log"
 
-listen "var/src/whatsmyx/tmp/sockets/unicorn.sock"
+listen "/tmp/unicorn.wmx.sock"
 worker_processes 2
 timeout 30
